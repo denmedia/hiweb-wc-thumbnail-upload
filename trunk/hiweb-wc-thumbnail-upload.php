1f357b5941a4d44d43d54cb46dcf00b9a99461eb
DenMedia <d9251132826@gmail.com>
2017-06-23 17:28:07 +0300

    1.0.1.0

--- trunk/hiweb-wc-thumbnail-upload.php
+++ trunk/hiweb-wc-thumbnail-upload.php
@@ -2,7 +2,7 @@
 	/**
 	 * Plugin Name: hiWeb WooCommerce Thumbnail Upload
 	 * Description: Upload pictures of products and categories directly in the list of goods and categories using the drag & drop method
-	 * Version: 1.0.0.0
+	 * Version: 1.0.1.0
 	 * Author: Den Media
 	 * Author URI: http://hiweb.moscow
 	 */
